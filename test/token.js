f7f5fb5680d3ef7031200837f67cae6d5d8c424e
fishseabowl <jjmm1100@gmail.com>
2023-04-15 16:53:55 -0700

    Add console.log to show all account address

--- test/token.js
+++ test/token.js
@@ -2,13 +2,20 @@ const { expect } = require("chai");
 
 describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+    const owner = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("Token");
 
     const hardhatToken = await Token.deploy();
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    const ownerBalance = await hardhatToken.balanceOf(owner[0].address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+
+    console.log('Number of singers:', owner.length);
+
+    for (let i=0; i< owner.length; i++) {
+      const address = await owner[i].getAddress();
+      console.log('Owner',i, address);
+    }
   });
 });
